913d5ee4b14f614c91d759ad47386e5e9d47b17c
Nic Ferrier <nic@ferrier.me.uk>
2018-09-11 14:05:27 +0100

    have pgbootdemo handle new startup correctly

--- pgbootdemo.js
+++ pgbootdemo.js
@@ -14,12 +14,12 @@ const rl = readline.createInterface({
     terminal: false,
 });
             
-function devCli(app) {
+function devCli(dbConfig) {
     rl.question("> ", (command) => {
         console.log("got a command");
         switch (command) {
         case "psql":
-            app.psqlSpawn(devCli);
+            dbConfig.psqlSpawn(devCli);
             break;
         case "help":
             console.log("this is a simple cli allowing launching of psql");
@@ -42,6 +42,40 @@ const options = {
     cli: false
 };
 
+const dbConfig = {};
+
+
+// Listen for the dbUp event to receive the connection pool
+pgBoot.events.on("dbUp", async dbDetails => {
+    let { pgPool, psql } = dbDetails;
+
+    // when we get the pool make a query method available
+    dbConfig.query = async function (sql, parameters) {
+        let client = await pgPool.connect();
+        try {
+            let result = await client.query(sql, parameters);
+            return result;
+        }
+        catch (e) {
+            return {
+                dberror: e
+            };
+        }
+        finally {
+            client.release();
+        }
+    };
+
+    dbConfig.psqlSpawn = psql;
+});
+
+pgBoot.events.on("dbPostInit", () => {
+    console.log("pgboot webapp listening on ", port);
+    if (options.cli) {
+        devCli(dbConfig);
+    }
+});
+
 // Main
 pgBoot.boot(port, {
     dbDir: path.join(__dirname, "dbfiles"),
@@ -61,6 +95,9 @@ pgBoot.boot(port, {
 
         // Dummy query function until we have a db up
         app.query = async function (sql, parameters) {
+            if (dbConfig.query !== undefined) {
+                return dbConfig.query(sql, parameters);
+            }
             throw new Error("no db connection yet");
         };
 
@@ -75,38 +112,6 @@ pgBoot.boot(port, {
             });
         }
         // end psqlweb
-
-
-        // Listen for the dbUp event to receive the connection pool
-        pgBoot.events.on("dbUp", async dbDetails => {
-            let { pgPool, psql } = dbDetails;
-
-            // when we get the pool make a query method available
-            app.query = async function (sql, parameters) {
-                let client = await pgPool.connect();
-                try {
-                    let result = await client.query(sql, parameters);
-                    return result;
-                }
-                catch (e) {
-                    return {
-                        dberror: e
-                    };
-                }
-                finally {
-                    client.release();
-                }
-            };
-
-            app.psqlSpawn = psql;
-        });
-
-        pgBoot.events.on("dbPostInit", () => {
-            console.log("pgboot webapp listening on ", port);
-            if (options.cli) {
-                devCli(app);
-            }
-        });
 
         app.get("/status", async function (req, res) {
             let query = "SELECT * FROM nictest;";
